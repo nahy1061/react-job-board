eca45d780329f5adc07f54f58b370923ce98ba12
Naheed <naheed.official99@gmail.com>
2026-06-12 14:57:27 +0500

    CSS changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,34 +1,38 @@
-import React from 'react'
-import { Bookmark } from 'lucide-react'
+import React from "react";
+import { Bookmark } from "lucide-react";
 
 const Card = (props) => {
   return (
     <div className="card">
+      <div className="top">
+        <img src={props.brandLogo} alt="logo" />
+        <button>
+          Save <Bookmark size={12} />
+        </button>
+      </div>
 
-        <div className="top">
-            <img src={props.brandLogo} alt="logo" />
-            <button>Save <Bookmark size={12} /></button>
+      <div className="core">
+        <h3>
+          {props.companyName} <span>{props.datePosted}</span>
+        </h3>
+        <h2>{props.position}</h2>
+        <div className="tags">
+          <h4>{props.tag1}</h4>
+          <h4>{props.tag2}</h4>
         </div>
+      </div>
 
-        <div className="core">
-            <h3>{props.companyName} <span>{props.datePosted}</span></h3>
-            <h2>{props.position}</h2>
-            <div className="tags">
-                <h4>{props.tag1}</h4>
-                <h4>{props.tag2}</h4>
-            </div>
+      <div className="bottom">
+        <div>
+          <h3>{props.pay}</h3>
+          <p>{props.location}</p>
         </div>
-
-            <div className="bottom">
-                <div>
-                    <h3>{props.pay}</h3>
-                    <p>{props.location}</p>
-                </div>
-                <a href="#"><button>Apply Now</button></a>
-            </div>
-     
+        <a href="#">
+          <button>Apply Now</button>
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
